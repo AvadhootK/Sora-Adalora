c4968e1321d64c6376646476d1fe6a7559adc894
Avadhoot K <avadhootkulkarni06@gmail.com>
2024-09-29 13:45:10 -0400

    update

--- Sora_final/run_glue.py
+++ Sora_final/run_glue.py
@@ -51,7 +51,7 @@ from transformers import get_linear_schedule_with_warmup
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 # check_min_version("4.24.0")
-
+os.environ["WANDB_DISABLED"] = "true"
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/text-classification/requirements.txt")
 
 task_to_keys = {
